c330a16ceda1f585982d2421aff67707f8d8b596
kobainu <kobadai0520.1@gmail.com>
2023-02-03 13:56:05 +0900

    add :サインアップ時にnameを登録する機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
   # ログイン後の遷移先
   def after_sign_in_path_for(resource)
@@ -8,8 +9,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # 入力フォームからアカウント名情報をDBに保存するために追加
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    added_attrs = [ :email, :name, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
   }
 
   devise_scope :user do
