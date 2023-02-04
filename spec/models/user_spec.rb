d49bf947c215eceffe1614dde159a22e4b6eb319
kobainu <kobadai0520.1@gmail.com>
2023-02-04 23:02:22 +0900

    add: ModelSpec追加'名前、メール、パスワードがある場合、有効である'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
+
+  it '名前、メール、パスワードがある場合、有効である' do
+    user = User.new(
+      name: 'test_user',
+      email: 'test@example.com',
+      password: 'password'
+    )
+    expect(user).to be_valid
+  end
+  
+  # it "名前がない場合、無効である" do
+  #   user = User.new(
+  #     name: nil,
+  #     email: 'test@example.com',
+  #     password: 'password'
+  #   )
+  #   expect(user).to be_valid
+  # end
+
+  # it "メールアドレスがない場合、無効である" do
+  #   user = User.new(
+  #     name: 'test_user',
+  #     email: 'test@example.com',
+  #     password: 'password'
+  #   )
+  #   expect(user).to be_valid
+  # end
+
+  # it "重複したメールアドレスの場合、無効である" do
+  #   user = User.new(
+  #     name: 'test_user',
+  #     email: 'test@example.com',
+  #     password: 'password'
+  #   )
+  #   expect(user).to be_valid
+  # end
+
+  # it "パスワードがない場合、無効である" do
+  #   user = User.new(
+  #     name: 'test_user',
+  #     email: 'test@example.com',
+  #     password: 'password'
+  #   )
+  #   expect(user).to be_valid
+  # end
+
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /profile" do
-    it "returns http success" do
-      get "/users/profile"
-      expect(response).to have_http_status(:success)
-    end
-  end
+# RSpec.describe "Users", type: :request do
+#   describe "GET /profile" do
+#     it "プロフィール編集画面に遷移すること" do
+#       get users_profile_path
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-end
+# end
