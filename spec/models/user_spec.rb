490e1d22a5e6f69dfacb155e881f71c5e7b17877
kobainu <kobadai0520.1@gmail.com>
2023-02-05 00:20:09 +0900

    add: ModelSpec追加'重複したメールアドレスの場合、無効である'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,14 +31,20 @@ RSpec.describe User, type: :model do
     expect(user.errors[:email]).to include("can't be blank")
   end
 
-  # it '重複したメールアドレスの場合、無効である' do
-  #   user = User.new(
-  #     name: 'test_user',
-  #     email: 'test@example.com',
-  #     password: 'password'
-  #   )
-  #   expect(user).to be_valid
-  # end
+  it '重複したメールアドレスの場合、無効である' do
+    user = User.create(
+      name: 'test_user',
+      email: 'test@example.com',
+      password: 'password'
+    )
+    other_user = User.new(
+      name: 'other_user',
+      email: 'test@example.com',
+      password: 'password'
+    )
+    other_user.valid?
+    expect(other_user.errors[:email]).to include("has already been taken")
+  end
 
   # it 'パスワードがない場合、無効である' do
   #   user = User.new(
