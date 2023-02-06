f208bf9127f11511fe9febbb4957b35abe7cc4bc
kobainu <kobadai0520.1@gmail.com>
2023-02-06 15:36:47 +0900

    add: usersテーブルにintroductionカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_05_121452) do
+ActiveRecord::Schema.define(version: 2023_02_06_063407) do
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_02_05_121452) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
     t.string "image"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
