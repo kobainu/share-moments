281b9b62ce304e45d066b18dacf27bbe12eb1b94
kobainu <kobadai0520.1@gmail.com>
2023-02-07 18:57:48 +0900

    add: ユーザーの詳細画面を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id)
+  end
+
   def profile
     @user = User.find(current_user.id)
   end
