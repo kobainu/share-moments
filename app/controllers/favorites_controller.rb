26333af80e60300150ca3eb4a26199ed4d4533ce
kobainu <kobadai0520.1@gmail.com>
2023-02-08 10:24:42 +0900

    update: お気に入り投稿機能を非同期処理化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,15 @@
 class FavoritesController < ApplicationController
+  before_action :post_find
   def create
     Favorite.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to posts_path
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:id])
     favorite.destroy
-    redirect_to posts_path
+  end
+
+  def post_find
+    @post = Post.find(params[:id])
   end
 end
