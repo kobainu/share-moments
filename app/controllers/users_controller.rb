fc66ef86f9ee9b690c78f70b68cd1f5d9e98652f
kobainu <kobadai0520.1@gmail.com>
2023-02-08 17:27:56 +0900

    add: フォロー・フォロワー機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = Post.where(user_id: @user.id)
     @user_favorites = @user.favorites
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def profile
@@ -19,6 +21,16 @@ class UsersController < ApplicationController
       flash.now[:notice] = "更新に失敗しました"
       render profile_users_path
     end
+  end
+
+  def follows
+    user = User.find(params[:id])
+    @users = user.following_user
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.follower_user
   end
 
   private
