1dfe8b26ac5a98b8380a8d249943fa18446723a0
kobainu <kobadai0520.1@gmail.com>
2023-02-09 15:44:24 +0900

    add: commentsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_08_064302) do
+ActiveRecord::Schema.define(version: 2023_02_09_064301) do
+
+  create_table "comments", charset: "utf8mb3", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "favorites", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id", null: false
