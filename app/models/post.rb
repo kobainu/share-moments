bd6a45606a6b15f7defb47bba942cec099584b0a
kobainu <kobadai0520.1@gmail.com>
2023-02-11 17:55:06 +0900

    add: tag_mapsテーブルを通してpostsテーブルとtagsテーブルを関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :tag_maps, dependent: :destroy
+  has_many :tags, through: :tag_maps
   mount_uploader :photo, ImageUploader
 end
