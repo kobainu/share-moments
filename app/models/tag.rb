30ebddfdb488d93e9d84a44555b3f305223405fd
kobainu <kobadai0520.1@gmail.com>
2023-03-01 21:58:32 +0900

    add: コメントフォームに文字数制限を追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,4 @@
 class Tag < ApplicationRecord
   has_many :tag_maps, dependent: :destroy
   has_many :posts, through: :tag_maps
-
-  validates :tag_name, length: { maximum: 10 }
 end
--- spec/factories/tags.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :tag do
-    tag_name { 't' * 10 }
-  end
-end
